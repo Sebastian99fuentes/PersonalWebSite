6bb25cc87c93c9f3edbc6fab96a7b8530e15070b
Sebastian99fuentes <80362481+Sebastian99fuentes@users.noreply.github.com>
2023-02-13 10:49:10 -0500

    primer comiitt

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+// Load the full build.
+const _ = require('lodash');
+// Load the core build.
+// var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+var fp = require('lodash/fp');
+ 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// // Load method categories.
+// var array = require('lodash/array');
+// var object = require('lodash/fp/object');
+ 
+// // Cherry-pick methods for smaller browserify/rollup/webpack bundles.
+// var at = require('lodash/at');
+// var curryN = require('lodash/fp/curryN');
+
+// var global variable 
+
+// varss
+let posts =[];
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// varss
+
+// ROUTES
+
+// para poder renderizar nuestra vista tenemos que hacer un get de nuestras vistas especificando  la ruta
+app.get("/",function(req, res){
+// Enviamos un obj con el valor y la key
+res.render("home",{startingContent:homeStartingContent, arrayPost:posts})
+});
+app.get("/contact",function(req, res){
+  // Enviamos un obj con el valor y la key
+  res.render("contact",{contactContent:contactContent})
+});
+app.get("/about",function(req, res){
+  // Enviamos un obj con el valor y la key
+  res.render("about",{aboutContent:aboutContent})
+});
+app.get("/compose",function(req, res){
+  // Enviamos un obj con el valor y la key
+  res.render("compose")
+});
+
+app.get("/posts/:match",function(req, res){
+
+  // console.log(req.params.matchfound)
+
+  posts.forEach(element => {
+    if(_.lowerCase(element.title)==_.lowerCase(req.params.match)){
+      console.log("mathc found")
+        // Enviamos un obj con el valor y la key
+      res.render("post",{titleContent:_.lowerCase(element.title), contentPost:element.content})
+    }
+  });
+  });
+
+// ROUTES
+
+
+app.post("/compose",function(req, res){
+  console.log(req.body.titlecont)
+  const objPost= {
+    title: req.body.titlecont,
+    content: req.body.postContent
+  }
+
+posts.push(objPost)
+
+  res.redirect("/");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
